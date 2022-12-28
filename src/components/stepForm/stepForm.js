2dc038ed5a4280826032143e1e07df25ab9561d7
johngaynor <86327580+johngaynor@users.noreply.github.com>
2022-12-28 17:39:45 -0500

    form progress

--- src/components/stepForm/stepForm.js
+++ src/components/stepForm/stepForm.js
@@ -5,34 +5,61 @@ import styles from "./stepForm.css";
 function StepForm() {
   useEffect(() => {
     const multiStepForm = document.querySelector("[data-multi-step]");
-    const formSteps = multiStepForm.querySelectorAll("[data-step]");
-    console.log(formSteps);
+    const formSteps = Array.from(multiStepForm.querySelectorAll("[data-step]"));
+
     let currentStep = formSteps.findIndex((step) => {
       return step.classList.contains("active");
     });
-    console.log(currentStep);
+    // console.log(currentStep);
 
-    // if (isNaN(currentStep)) {
-    //   formSteps.find();
-    // }
+    if (currentStep < 0) {
+      currentStep = 0;
+      formSteps[currentStep].classList.add("active");
+      showCurrentStep();
+    }
+
+    multiStepForm.addEventListener("click", (e) => {
+      if (e.target.matches("[data-next]")) {
+        currentStep += 1;
+      } else if (e.target.matches("[data-previous]")) {
+        currentStep -= 1;
+      }
+      showCurrentStep();
+    });
+
+    function showCurrentStep() {
+      formSteps.forEach((step, index) => {
+        step.classList.toggle("active", index === currentStep);
+      });
+    }
   });
+
+  // problem ended up being that nodeList doesn't work with findIndex, had to convert it to an array first
 
   return (
     <form data-multi-step className="w-75 mx-auto p-5 text-black">
-      <div className="form-card active" data-step>
+      <div className="form-card" data-step>
         <p>1</p>
-        <button type="button">Next</button>
+        <button type="button" data-next>
+          Next
+        </button>
       </div>
       <div className="form-card" data-step>
         <p>2</p>
 
-        <button type="button">Previous</button>
-        <button type="button">Next</button>
+        <button type="button" data-previous>
+          Previous
+        </button>
+        <button type="button" data-next>
+          Next
+        </button>
       </div>
       <div className="form-card" data-step>
         <p>3</p>
 
-        <button type="button">Previous</button>
+        <button type="button" data-previous>
+          Previous
+        </button>
         <button type="submit">Submit</button>
       </div>
     </form>
