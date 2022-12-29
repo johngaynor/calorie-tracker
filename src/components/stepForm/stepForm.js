81063fa29f72be0c37f447d1db605bed17e7fc2a
johngaynor <86327580+johngaynor@users.noreply.github.com>
2022-12-28 19:43:33 -0500

    new form progress

--- src/components/stepForm/stepForm.js
+++ src/components/stepForm/stepForm.js
@@ -1,5 +1,18 @@
-import { useEffect } from "react";
-
+import React, { useState, useEffect } from "react";
+import firebase from "../../utilities/firebase";
+import {
+  MDBInput,
+  MDBRow,
+  MDBCol,
+  MDBContainer,
+  MDBBtn,
+  MDBTable,
+  MDBTableHead,
+  MDBProgress,
+  MDBProgressBar,
+  MDBTextArea,
+} from "mdb-react-ui-kit";
+import { Form } from "react-bootstrap";
 import styles from "./stepForm.css";
 
 function StepForm() {
@@ -10,7 +23,6 @@ function StepForm() {
     let currentStep = formSteps.findIndex((step) => {
       return step.classList.contains("active");
     });
-    // console.log(currentStep);
 
     if (currentStep < 0) {
       currentStep = 0;
@@ -36,33 +48,103 @@ function StepForm() {
 
   // problem ended up being that nodeList doesn't work with findIndex, had to convert it to an array first
 
+  // left off on the video at 16:17
+
   return (
-    <form data-multi-step className="w-75 mx-auto p-5 text-black">
-      <div className="form-card" data-step>
-        <p>1</p>
-        <button type="button" data-next>
-          Next
-        </button>
-      </div>
-      <div className="form-card" data-step>
-        <p>2</p>
+    <MDBContainer>
+      <h3 className="p-3 basic-header text-white">New Recipe Form</h3>
 
-        <button type="button" data-previous>
-          Previous
-        </button>
-        <button type="button" data-next>
-          Next
-        </button>
-      </div>
-      <div className="form-card" data-step>
-        <p>3</p>
+      <MDBContainer fluid className="w-75 recipe-form">
+        <form data-multi-step className="mx-auto p-3 text-black">
+          <div className="form-card" data-step>
+            <MDBProgress style={{ height: "10px" }}>
+              <MDBProgressBar
+                striped
+                animated
+                width="33"
+                valuemin={0}
+                valuemax={100}
+              />
+            </MDBProgress>
+            <MDBRow className="pt-5">
+              <MDBInput
+                // id="input-recipe"
+                label="Recipe Name"
+                type="text"
+                onChange={(e) => {
+                  //   setRecipeName(e.target.value);
+                }}
+                // value={recipeName}
+                contrast
+              />
+            </MDBRow>
+            <MDBRow className="bg-primary d-flex justify-content-between pt-3">
+              <MDBCol className="p-0 recipe-form-category">
+                <Form.Select
+                  aria-label="Default select"
+                  size="md"
+                  onChange={(e) => {
+                    // setUnit(e.target.value);
+                  }}
+                >
+                  <option>Category</option>
+                  <option value="breakfast">Breakfast</option>
+                  <option value="lunch">Lunch</option>
+                  <option value="dinner">Dinner</option>
+                  <option value="snack">Snack</option>
+                </Form.Select>
+              </MDBCol>
+              <MDBCol className="p-0 recipe-form-desc">
+                <MDBTextArea
+                  // id="input-recipe"
+                  label="Description"
+                  type="text"
+                  onChange={(e) => {
+                    //   setRecipeName(e.target.value);
+                  }}
+                  // value={recipeName}
+                  contrast
+                  rows={4}
+                />
+              </MDBCol>
+            </MDBRow>
+            <MDBRow>
+              {/* <button type="button" data-next>
+              Next
+            </button> */}
+              <MDBBtn
+                outline
+                color="light"
+                className="border-1"
+                type="button"
+                //   id="recipe-submit"
+                //   onClick={createRecipe}
+              >
+                Next
+              </MDBBtn>
+            </MDBRow>
+          </div>
+          <div className="form-card" data-step>
+            <p>2</p>
 
-        <button type="button" data-previous>
-          Previous
-        </button>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+            <button type="button" data-previous>
+              Previous
+            </button>
+            <button type="button" data-next>
+              Next
+            </button>
+          </div>
+          <div className="form-card" data-step>
+            <p>3</p>
+
+            <button type="button" data-previous>
+              Previous
+            </button>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </MDBContainer>
+    </MDBContainer>
   );
 }
 
