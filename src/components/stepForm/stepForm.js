0160f171fccb03436d7b17181188fe7be85011ba
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-01-04 17:03:38 -0500

    form progress

--- src/components/stepForm/stepForm.js
+++ src/components/stepForm/stepForm.js
@@ -14,10 +14,13 @@ import {
   MDBTabsContent,
 } from "mdb-react-ui-kit";
 import { Form } from "react-bootstrap";
+import IngredientList from "../recipes/newRecipe/ingredientList/ingredientList";
 import styles from "./stepForm.css";
 
 function StepForm() {
+  const [formStep, setFormStep] = useState(0);
   const [ingredientList, setIngredientList] = useState("");
+  // these are for recipe/ingredient submission
   const [recipeName, setRecipeName] = useState("");
   const [ingredientName, setIngredientName] = useState("");
   const [size, setSize] = useState("");
@@ -32,31 +35,62 @@ function StepForm() {
     const multiStepForm = document.querySelector("[data-multi-step]");
     const formSteps = Array.from(multiStepForm.querySelectorAll("[data-step]"));
 
-    let currentStep = formSteps.findIndex((step) => {
-      return step.classList.contains("active");
-    });
-
-    if (currentStep < 0) {
-      currentStep = 0;
-      formSteps[currentStep].classList.add("active");
-      showCurrentStep();
-    }
-
+    // event listeners to update formStep
     multiStepForm.addEventListener("click", (e) => {
       if (e.target.matches("[data-next]")) {
-        currentStep += 1;
+        // checking to see if everything is filled out
+        const inputs = [...formSteps[formStep].querySelectorAll("input")];
+        const allValid = inputs.every((input) => input.reportValidity());
+        if (allValid) {
+          setFormStep(formStep + 1);
+        }
       } else if (e.target.matches("[data-previous]")) {
-        currentStep -= 1;
+        setFormStep(formStep - 1);
       }
-      showCurrentStep();
+      window.scrollTo(0, 0);
     });
 
-    function showCurrentStep() {
-      formSteps.forEach((step, index) => {
-        step.classList.toggle("active", index === currentStep);
-      });
-    }
+    // applying the active class to whatever index matches formStep
+    formSteps.forEach((step, index) => {
+      step.classList.toggle("active", index === formStep);
+    });
   });
+
+  // function to submit recipes
+  const submitIngredient = () => {
+    const newIngredientRef = firebase.database().ref("add-ingredient");
+    if (
+      ingredientName === "" ||
+      size === "" ||
+      unit === "Unit" ||
+      unit === "" ||
+      cal === "" ||
+      protein === "" ||
+      carbs === "" ||
+      fat === ""
+    ) {
+      alert("please fill all fields before submitting.");
+    } else {
+      let newIngredient = {
+        name: ingredientName,
+        size: size,
+        unit: unit,
+        cal: cal,
+        protein: protein,
+        carbs: carbs,
+        fat: fat,
+        add: true,
+      };
+
+      newIngredientRef.push(newIngredient);
+      setIngredientName("");
+      setSize("");
+      setCal("");
+      setProtein("");
+      setCarbs("");
+      setFat("");
+    }
+  };
 
   // problem ended up being that nodeList doesn't work with findIndex, had to convert it to an array first
 
@@ -88,6 +122,7 @@ function StepForm() {
                   }}
                   // value={recipeName}
                   contrast
+                  required
                 />
               </MDBCol>
               <MDBCol className="recipe-form-category">
@@ -97,6 +132,7 @@ function StepForm() {
                   onChange={(e) => {
                     // setUnit(e.target.value);
                   }}
+                  // need to set up some sort of logic gate for this to catch
                 >
                   <option>Category</option>
                   <option value="breakfast">Breakfast</option>
@@ -109,7 +145,7 @@ function StepForm() {
             <MDBRow>
               <MDBCol>
                 <MDBTextArea
-                  label="Description"
+                  label="Description (optional)"
                   type="text"
                   // onChange={(e) => {
                   //     setRecipeName(e.target.value);
@@ -150,10 +186,10 @@ function StepForm() {
                   label="Ingredient Name"
                   type="text"
                   onChange={(e) => {
-                    //   setIngredientName(e.target.value);
+                    setIngredientName(e.target.value);
                   }}
                   contrast
-                  // value={ingredientName}
+                  value={ingredientName}
                 />
               </MDBCol>
               <MDBCol>
@@ -162,9 +198,9 @@ function StepForm() {
                   type="number"
                   label="Serving Size"
                   onChange={(e) => {
-                    //   setSize(e.target.value);
+                    setSize(e.target.value);
                   }}
-                  // value={size}
+                  value={size}
                   contrast
                 />
               </MDBCol>
@@ -172,9 +208,8 @@ function StepForm() {
                 <Form.Select
                   aria-label="Default select"
                   size="md"
-                  // className="form-meal"
                   onChange={(e) => {
-                    //   setUnit(e.target.value);
+                    setUnit(e.target.value);
                   }}
                 >
                   <option>Unit</option>
@@ -192,9 +227,9 @@ function StepForm() {
                   type="number"
                   label="Calories"
                   onChange={(e) => {
-                    //   setCal(e.target.value);
+                    setCal(e.target.value);
                   }}
-                  // value={cal}
+                  value={cal}
                   contrast
                 />
               </MDBCol>
@@ -204,9 +239,9 @@ function StepForm() {
                   type="number"
                   label="Protein"
                   onChange={(e) => {
-                    //   setProtein(e.target.value);
+                    setProtein(e.target.value);
                   }}
-                  // value={protein}
+                  value={protein}
                   contrast
                 />
               </MDBCol>
@@ -217,9 +252,9 @@ function StepForm() {
                   label="Carbs"
                   contrast
                   onChange={(e) => {
-                    //   setCarbs(e.target.value);
+                    setCarbs(e.target.value);
                   }}
-                  // value={carbs}
+                  value={carbs}
                 />
               </MDBCol>
               <MDBCol>
@@ -229,12 +264,30 @@ function StepForm() {
                   label="Fat"
                   contrast
                   onChange={(e) => {
-                    //   setFat(e.target.value);
+                    setFat(e.target.value);
                   }}
-                  // value={fat}
+                  value={fat}
                 />
               </MDBCol>
             </MDBRow>
+            <MDBRow className="mt-4">
+              <MDBCol>
+                <MDBBtn
+                  outline
+                  color="light"
+                  type="button"
+                  className="border-1 bg-danger w-100"
+                  onClick={submitIngredient}
+                >
+                  Add Ingredient
+                </MDBBtn>
+              </MDBCol>
+            </MDBRow>
+            <MDBRow className="mb-5 w-75 mx-auto">
+              <h3 className="text-white mt-3">Ingredients:</h3>
+              <IngredientList></IngredientList>
+            </MDBRow>
+
             <MDBRow className="recipe-form-btns">
               <MDBBtn
                 outline
