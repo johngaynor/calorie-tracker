fb375dab21676cfb444122add62b68b05a0498c2
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-01-16 19:30:13 -0500

    dashboard progress

--- src/components/macroCalcDash/macroMeter/macroMeter.js
+++ src/components/macroCalcDash/macroMeter/macroMeter.js
@@ -1,19 +1,62 @@
-import { MDBContainer } from "mdb-react-ui-kit";
+import { MDBContainer, MDBRow, MDBCol } from "mdb-react-ui-kit";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBreadSlice,
+  faChartPie,
+  faCow,
+  faEgg,
+} from "@fortawesome/free-solid-svg-icons";
 import styles from "./macroMeter.css";
 
 function MacroMeter() {
   //   let circleProgress = document.querySelector(".progress-inner");
 
   return (
-    <MDBContainer>
-      <div className="progress">
-        {/* // <div className="progress-inner circle"></div>
-        // <div className="progress-center circle"></div>
-        // <div className="progress-outer circle"></div>
-        // <span className="progress-value"></span> */}
-        <div className="pie text-black animate">60%</div>
-        <div className="progress-center"></div>
-      </div>
+    <MDBContainer className="bg-white d-flex p-0 justify-content-center align-items-center">
+      <MDBContainer className="progress-container w-50">
+        <div className="progress-circle">
+          <div className="pie"></div>
+          <div className="pie-center"></div>
+        </div>
+      </MDBContainer>
+      <MDBContainer className="macro-percents my-2 text-muted">
+        <div className="macro-small">
+          <div className="macro-small-icon cal">
+            <FontAwesomeIcon icon={faChartPie} />
+          </div>
+          <div className="percent-text">
+            <p className="m-0">calories</p>
+            <p className="fw-bold">100/200</p>
+          </div>
+        </div>
+        <div className="macro-small">
+          <div className="macro-small-icon protein">
+            <FontAwesomeIcon icon={faCow} />
+          </div>
+          <div className="percent-text">
+            <p className="m-0">protein (g)</p>
+            <p className="fw-bold">100/200</p>
+          </div>
+        </div>
+        <div className="macro-small">
+          <div className="macro-small-icon carbs">
+            <FontAwesomeIcon icon={faBreadSlice} />
+          </div>
+          <div className="percent-text">
+            <p className="m-0">carbs (g)</p>
+            <p className="fw-bold">100/200</p>
+          </div>
+        </div>
+        <div className="macro-small">
+          <div className="macro-small-icon fat">
+            <FontAwesomeIcon icon={faEgg} />
+          </div>
+          <div className="percent-text">
+            <p className="m-0">fat (g)</p>
+            <p className="fw-bold">100/200</p>
+          </div>
+        </div>
+      </MDBContainer>
     </MDBContainer>
   );
 }
