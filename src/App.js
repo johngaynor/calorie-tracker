60db3d7337a4382022f29fc978e0b7caaa68b9ce
johngaynor <jgaynr@icloud.com>
2023-06-30 15:27:49 -0400

    got the initial setup for context

--- src/App.js
+++ src/App.js
@@ -8,21 +8,24 @@ import Recipes from "./pages/recipes/recipes";
 import NewRecipe from "./pages/newRecipe/newRecipe";
 import { Route, Routes } from "react-router";
 import Login from "./components/login/login";
+import { AuthProvider } from "./utilities/auth/authContext";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation></Navigation>
-      <Login></Login>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/recipes" element={<Recipes />} />
-        <Route path="/foods" element={<Foods />} />
-        <Route path="/new-food" element={<NewFood />} />
-        <Route path="/new-recipe" element={<NewRecipe />} />
-      </Routes>
-      <Footer></Footer>
-    </div>
+    <AuthProvider>
+      <div className="App">
+        <Navigation></Navigation>
+        <Login></Login>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/foods" element={<Foods />} />
+          <Route path="/new-food" element={<NewFood />} />
+          <Route path="/new-recipe" element={<NewRecipe />} />
+        </Routes>
+        <Footer></Footer>
+      </div>
+    </AuthProvider>
   );
 }
 
